5b96888ddf0647f86f750e0f96c5edfc611ae5a7
Msgasu <edamg123@gmail.com>
2024-07-24 13:49:00 +0000

    removed unused variable, checked for roles to determine login redirection

--- action/register_user_action.php
+++ action/register_user_action.php
@@ -13,13 +13,20 @@ if (isset($_POST['submit'])) {
 
     // Prepare and bind parameters
     $stmt = $mysqli->prepare("INSERT INTO users (`f_name`, `l_name`, `email`, `passwd`, `role_id`) VALUES (?, ?, ?, ?, ?)");
-    $role_id = 1; // Default role ID
+    $role_id = 2; // Default role ID
     $stmt->bind_param("ssssi", $first_name, $last_name, $email, $hash, $role_id);
 
    
     if ($stmt->execute()) {
-        header("Location:../login/login_admin.php");
-        exit(); 
+        if ($role_id ==1 ){
+            header("Location:../login/login_admin.php");
+            exit(); 
+        }
+        else if($role_id == 2){
+            header("Location:../login/login_student.php");
+            exit(); 
+        }
+       
     } else {
         echo ("user not registered, somethings' wrong");
         die("Error: ". $mysqli->error);
