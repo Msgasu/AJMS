4146e4699cb0992bce386351283b21f32358ee9a
AngBenn <angela.benning@ashesi.edu.gh>
2024-07-31 23:20:48 +0000

    try

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -116,7 +116,7 @@
             margin-left: 260px;
             margin-right: 280px;
             padding: 20px 0;
-            height: calc(89.5vh - 28px);
+            height: calc(89.2vh - 27px);
             overflow-y: auto;
             display: flex;
             flex-direction: column;
