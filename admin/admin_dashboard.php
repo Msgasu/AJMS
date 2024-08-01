087ef6b174ef1c38bfe367e76a26c36a9b17f1b4
AngBenn <angela.benning@ashesi.edu.gh>
2024-08-01 01:12:26 +0000

    trying sth

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -311,36 +311,36 @@
         </div>
     </main>
 
-    <!-- Notifications Section in a Card -->
     <div class="card notifications card-special">
-        <div class="card-body">
+        <div class="card-body scrollable-notifications">
             <h2>Notifications</h2>
             <ul class="list-group list-group-flush">
                 <li class="list-group-item">
                     <strong>Today</strong>
-                    <span>Meetings with involved parties</span>
-                    <span>Committee Meeting</span>
-                    <span>Deliberation meeting</span>
+                    <span><i class="fas fa-user-friends"></i> Meetings with involved parties</span><br>
+                    <span><i class="fas fa-calendar-alt"></i> Committee Meeting</span><br>
+                    <span><i class="fas fa-clock"></i> Deliberation meeting</span>
                 </li>
                 <li class="list-group-item">
                     <strong>Yesterday</strong>
-                    <span>Submitted Case to Dean</span>
-                    <span>Student ID 12345684</span>
+                    <span><i class="fas fa-upload"></i> Submitted Case to Dean</span><br>
+                    <span>BSIT 1A<br>Student ID 1235124614</span>
                 </li>
                 <li class="list-group-item">
                     <strong>December 22, 2024</strong>
-                    <span>Case Verdict</span>
-                    <span>Start 11</span>
-                    <span>Student ID 12345684</span>
+                    <span><i class="fas fa-gavel"></i> Case Verdict</span><br>
+                    <span>BSIT 1A<br>Student ID 1235124614</span><br>
+                    <span><i class="fas fa-envelope"></i> Sending out email</span><br>
+                    <span>BSIT 1A<br>Student ID 1235124614</span>
                 </li>
             </ul>
         </div>
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/all.min.js"></script>
 </body>
-
 </html>
 
