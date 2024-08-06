80b156c408bfd87d4476e80bea1870ca761529c4
AngBenn <angela.benning@ashesi.edu.gh>
2024-08-06 13:43:28 +0000

    testing

--- admin/case_submission.php
+++ admin/case_submission.php
@@ -200,7 +200,7 @@
             background-color: #28a745;
             color: white;
             font-size: medium;
-            width: 20%;
+            width: 40%;
             border: none;
             padding: 5px 5px;
             margin-left: 20px;
