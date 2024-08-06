ad4b0330128ade30f2c5d00a6b9e3168f1d37003
JanetElorm <janet.ashigbui@ashesi.edu.gh>
2024-08-06 23:21:11 +0000

    schedule

---
+++ action/schedule_meeting_action.php
@@ -0,0 +1,19 @@
+<?php
+include '../settings/connection.php';
+
+$title = $_POST['title'];
+$user_id = $_POST['user_id'];
+$date = $_POST['date'];
+$time = $_POST['time'];
+$location = $_POST['location'];
+
+$sql = "INSERT INTO meetings (title, user_id, date, time, location) VALUES ('$title', '$user_id', '$date', '$time', '$location')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Meeting scheduled successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error;
+}
+
+$conn->close();
+?>
