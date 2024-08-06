44db4e5a46e1a444b5b9f392ce09cc9a87209cf4
Msgasu <edamg123@gmail.com>
2024-08-06 18:36:38 +0000

    corrected redirection for logout

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -304,7 +304,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="../login/login_admin.php">
+                    <a class="nav-link" href="../login/logout.php">
                         <i class="fas fa-sign-out-alt"></i>
                         <span> Logout</span>
                     </a>
