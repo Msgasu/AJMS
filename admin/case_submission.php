fe04cda9014ee40059f9d8e33b83e266e322524e
AngBenn <angela.benning@ashesi.edu.gh>
2024-08-06 13:17:58 +0000

    testing

--- admin/case_submission.php
+++ admin/case_submission.php
@@ -82,7 +82,7 @@
 
         .sidebar {
             position: fixed;
-            top: 65px;
+            top: 66px;
             bottom: 0;
             left: -220px; /* Initially hidden */
             z-index: 100;
