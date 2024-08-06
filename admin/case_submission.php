da8f4f3a8fc8aa61dc1ab76042393abc04021313
AngBenn <angela.benning@ashesi.edu.gh>
2024-08-06 13:11:49 +0000

    trying out a different spacing

--- admin/case_submission.php
+++ admin/case_submission.php
@@ -82,7 +82,7 @@
 
         .sidebar {
             position: fixed;
-            top: 75px;
+            top: 83 px;
             bottom: 0;
             left: -220px; /* Initially hidden */
             z-index: 100;
