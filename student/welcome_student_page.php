34c89bf6966a48963fd459d1d778763a91d485e9
AngBenn <angela.benning@ashesi.edu.gh>
2024-08-07 21:02:43 +0000

    test

--- student/welcome_student_page.php
+++ student/welcome_student_page.php
@@ -35,6 +35,7 @@
             text-align: center;
             width: 100%;
             padding-bottom: 0;
+            margin-left: 10px;
         }
 
         .modal-title {
@@ -103,12 +104,10 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h3 class="modal-title" id="welcomeModalLabel">Welcome to the AJMS!</h3>
-                   
                     
                 </div>
-                <p><em>Please enter your details.</em></p>
                 <div class="modal-body">
-                    
+                    <p>Please enter your details.</p>
                     <form id="welcomeForm">
                         <div class="form-group">
                             <label for="roleSelect">Are you a Witness or a Victim?</label>
