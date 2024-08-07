535f171c530f0d68354b38eb025f286ff4958619
AngBenn <angela.benning@ashesi.edu.gh>
2024-08-07 20:44:58 +0000

    edit

--- student/welcome_student_page.php
+++ student/welcome_student_page.php
@@ -100,7 +100,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title" id="welcomeModalLabel">Welcome to the AJMS!</h4>
+                    <h3 class="modal-title" id="welcomeModalLabel">Welcome to the AJMS!</h3>
                 </div>
                 <div class="modal-body">
                     <p>Please enter your details.</p>
