dc07afb1dc412c5ad51a7ab38d869b8c36302454
AngBenn <angela.benning@ashesi.edu.gh>
2024-08-07 23:04:34 +0000

    done

--- student/case_submission.php
+++ student/case_submission.php
@@ -451,7 +451,7 @@
         <br>
             <div class="form-group">
                 <label for="emailCount" style="font-weight: bolder"><em>Enter the number of emails for involved parties.</em></label>
-                <input type="number" class="form-control" id="emailCount" min="1" placeholder="Enter number of emails" required>
+                <input type="number" class="form-control" id="emailCount" min="1" placeholder="Number of emails" required>
             </div>
             <div id="emailInputs"></div>
             
