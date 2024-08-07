7c98b07f44f5a0045a3938cc723b6d016511c27a
JanetElorm <janet.ashigbui@ashesi.edu.gh>
2024-08-07 22:50:23 +0000

    profile backend

---
+++ action/profile_page_action.php
@@ -0,0 +1,15 @@
+<?php
+include '../settings/connection.php';
+
+session_start();
+$user_id = $_SESSION['user_id'];
+
+// Fetch user data
+$sql = "SELECT f_name, l_name, email FROM users WHERE pid = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $user_id);
+$stmt->execute();
+$stmt->bind_result($f_name, $l_name, $email);
+$stmt->fetch();
+$stmt->close();
+$conn->close();
