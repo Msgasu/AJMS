bf59df39f93167435ae4b47bd697caeefd374ca3
Msgasu <edamg123@gmail.com>
2024-08-07 13:33:18 +0000

    modified file to include hiding ana d viewing of attached files as wella s capturing the stauts id to be changed

--- js/submitted_cases.js
+++ js/submitted_cases.js
@@ -1,22 +1,43 @@
+function toggleDocument(statementId) {
+    var docContainer = document.getElementById('doc-' + statementId);
+    var viewButton = docContainer.nextElementSibling;
+    var hideButton = viewButton.nextElementSibling;
+
+    if (docContainer.style.display === 'none') {
+        docContainer.style.display = 'block';
+        viewButton.style.display = 'none';
+        hideButton.style.display = 'inline';
+    } else {
+        docContainer.style.display = 'none';
+        viewButton.style.display = 'inline';
+        hideButton.style.display = 'none';
+    }
+}
+
 function toggleText(link) {
-    var text = link.previousElementSibling;
-    if (text.style.display === "none") {
-        text.style.display = "inline";
-        link.textContent = "Read Less";
+    var moreText = link.previousElementSibling;
+    if (moreText.style.display === 'none') {
+        moreText.style.display = 'inline';
+        link.innerHTML = 'Read Less';
     } else {
-        text.style.display = "none";
-        link.textContent = "Read More";
+        moreText.style.display = 'none';
+        link.innerHTML = 'Read More';
     }
 }
 
-function toggleMedia(button) {
-    var mediaContainer = button.nextElementSibling;
-    if (mediaContainer.style.display === "none") {
-        mediaContainer.style.display = "block";
-        button.textContent = "Hide Attached File";
-    } else {
-        mediaContainer.style.display = "none";
-        button.textContent = "View Attached File";
-    }
-}
+function updateStatus(selectElement, statementId) {
+    var statusId = selectElement.value;
 
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "../action/update_case_status.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send("statement_id=" + encodeURIComponent(statementId) + "&status_id=" + encodeURIComponent(statusId));
+
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            alert("Status updated successfully.");
+        } else {
+            alert("Failed to update status.");
+        }
+    };
+}
