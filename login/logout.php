4607a5b52ef3666918f1156365e4584626dac24f
Msgasu <edamg123@gmail.com>
2024-08-07 12:17:02 +0000

    modified redirection based on role id

--- login/logout.php
+++ login/logout.php
@@ -2,5 +2,12 @@
 session_start();
 unset($_SESSION['user_id']);
 unset($_SESSION['role_id']);
-header ("Location:login_admin.php");
+
+if ($_SESSION['role_id']== 1){
+    header ("Location:login_admin.php");
+}
+else{
+    header ("Location:login_student.php");
+}
+
 ?>
