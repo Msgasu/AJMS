80bcf6765044d7fc6a5bc91a50254cbea316ca98
AngBenn <angela.benning@ashesi.edu.gh>
2024-08-07 19:56:08 +0000

    removing the close button

--- student/welcome_student_page.php
+++ student/welcome_student_page.php
@@ -65,7 +65,7 @@
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="welcomeModalLabel">Welcome</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
