01fea4f7c107e090f9e5ce8241f8c88a3c6bbd70
AngBenn <angela.benning@ashesi.edu.gh>
2024-08-07 18:02:13 +0000

    edit

--- student/welcome_student_page.php
+++ student/welcome_student_page.php
@@ -88,9 +88,7 @@
                         </div>
                     </form>
                 </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                </div>
+                
             </div>
         </div>
     </div>
