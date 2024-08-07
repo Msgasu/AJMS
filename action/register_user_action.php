73c6431715e70f9ec6116160a4a9a355eab0503b
Msgasu <edamg123@gmail.com>
2024-08-07 17:36:04 +0000

    modified registration

--- action/register_user_action.php
+++ action/register_user_action.php
@@ -9,12 +9,14 @@ if (isset($_POST['submit'])) {
     $email = mysqli_real_escape_string($con, $_POST["email"]);
     $pass_1 = $_POST["password"];
     $pass_2 = $_POST["confirmPassword"];    
-    $hash = password_hash($pass_2, PASSWORD_DEFAULT); 
 
     // Check if passwords match
     if ($pass_1 !== $pass_2) {
         die("Passwords do not match.");
     }
+
+    // Hash the password
+    $hash = password_hash($pass_1, PASSWORD_DEFAULT);
 
     // Check if the email is in the admins table
     $checkAdminStmt = $con->prepare("SELECT email FROM admins WHERE email = ?");
@@ -27,22 +29,18 @@ if (isset($_POST['submit'])) {
     $checkAdminStmt->close();
 
     // Prepare and bind parameters for user insertion
-    $stmt = $con->prepare("INSERT INTO users (`f_name`, `l_name`, `email`, `passwd`, `role_id`) VALUES (?, ?, ?, ?, ?)");
+    $stmt = $con->prepare("INSERT INTO users (`f_name`, `l_name`, `email`, `passwd`, `role_id`, `first_login`) VALUES (?, ?, ?, ?, ?, TRUE)");
     $stmt->bind_param("ssssi", $first_name, $last_name, $email, $hash, $role_id);
 
     // Execute the statement
     if ($stmt->execute()) {
         // Redirect based on role_id
-        if ($role_id == 1) {
-            header("Location: ../login/login_admin.php");
-            exit(); 
-        } else if ($role_id == 2) {
-            header("Location: ../login/login_student.php");
-            exit(); 
-        }
+        
+            header("Location: ../login/login.php");
+        
+        exit();
     } else {
-        echo "User not registered, something's wrong.";
-        die("Error: " . $con->error);
+        echo "User not registered, something went wrong.";
     }
 
     $stmt->close();
