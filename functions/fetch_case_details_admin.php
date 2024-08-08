fd5f7b8e5dd9c184d2ed5dab65d73f431c7b1aea
Msgasu <edamg123@gmail.com>
2024-08-08 19:33:46 +0000

    read more read less

--- functions/fetch_case_details_admin.php
+++ functions/fetch_case_details_admin.php
@@ -24,32 +24,62 @@ function generateAndDisplayCards() {
         $participants = $participant_result->fetch_all(MYSQLI_ASSOC);
         $participant_stmt->close();
 
-        // Echo HTML card with a clickable link
+        // Generate HTML for each case card
+        $fullDescription = htmlspecialchars($case['statement_description']);
+        $shortDescription = strlen($fullDescription) > 100 ? substr($fullDescription, 0, 100) . '...' : $fullDescription;
+        $id = htmlspecialchars($case['id']);
+        
         echo '<div class="col-md-4">
- 
-                    <div class="card mb-4 shadow-sm card-custom">
-                        <div class="case-card-header" style="background-image: url(\'../images/Ashesi.jpg\');">
-                            <span>' . htmlspecialchars($case['status_name']) . '</span>
-                        </div>
-                        <div class="case-card-body">
-                                        <a href="case_hearing.php?case_id=' . htmlspecialchars($case['id']) . '" style="text-decoration: none;"> 
-                            <h5 class="card-title">Case ID: ' . htmlspecialchars($case['id']) . '</h5></a>
-                            <p>Description: ' . htmlspecialchars($case['statement_description']) . '</p>
-                            <p>Document: <a href="' . htmlspecialchars($case['document_url']) . '" target="_blank">View Document</a></p>
-                            <p>Victim: ' . htmlspecialchars($case['user_f_name']) . ' ' . htmlspecialchars($case['user_l_name']) . ' (' . htmlspecialchars($case['user_email']) . ')</p>
-                            <p>Involved Parties:</p>';
-
+                <div class="card mb-4 shadow-sm card-custom">
+                    <div class="case-card-header" style="background-image: url(\'../images/Ashesi.jpg\');">
+                        <span>' . htmlspecialchars($case['status_name']) . '</span>
+                    </div>
+                    <div class="case-card-body">
+                        <a href="case_hearing.php?case_id=' . $id . '" style="text-decoration: none;">
+                            <h5 class="card-title">Case ID: ' . $id . '</h5>
+                        </a>
+                        <p id="desc' . $id . '" class="case-description">' . $shortDescription . '</p>
+                        <p id="fullDesc' . $id . '" class="case-description" style="display:none;">' . $fullDescription . '</p>
+                        <button class="btn btn-link read-more" data-target="desc' . $id . '" data-toggle="fullDesc' . $id . '">Read More</button>
+                        <p>Document: <a href="' . htmlspecialchars($case['document_url']) . '" target="_blank">View Document</a></p>
+                        <p>Victim: ' . htmlspecialchars($case['user_f_name']) . ' ' . htmlspecialchars($case['user_l_name']) . ' (' . htmlspecialchars($case['user_email']) . ')</p>
+                        <p>Involved Parties:</p>';
+        
         foreach ($participants as $participant) {
             echo '<p>' . htmlspecialchars($participant['f_name'] . ' ' . $participant['l_name']) . ' (' . htmlspecialchars($participant['student_email']) . ') <br>Role: ' . htmlspecialchars($participant['participant']) . '</p>';
         }
-
+        
         echo '   <button class="btn btn-success">Schedule meeting</button>
                 </div>
             </div>
-      
-    </div>';
+        </div>';
     }
 
     $con->close();
 }
 ?>
+
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+    // Toggle description visibility
+    document.querySelectorAll('button.read-more').forEach(button => {
+        button.addEventListener('click', function() {
+            var targetId = this.getAttribute('data-target');
+            var toggleId = this.getAttribute('data-toggle');
+            
+            var shortDesc = document.getElementById(targetId);
+            var fullDesc = document.getElementById(toggleId);
+            
+            if (fullDesc.style.display === 'none') {
+                fullDesc.style.display = 'block';
+                shortDesc.style.display = 'none';
+                this.textContent = 'Read Less';
+            } else {
+                fullDesc.style.display = 'none';
+                shortDesc.style.display = 'block';
+                this.textContent = 'Read More';
+            }
+        });
+    });
+});
+</script>
