2d2bc2a6cb0d871cfe158523d6a66435c5c942ca
AngBenn <angela.benning@ashesi.edu.gh>
2024-08-09 04:49:21 +0000

    test

--- action/recommender.php
+++ action/recommender.php
@@ -45,7 +45,7 @@ if (isset($_POST['submit'])) {
     $suggestedVerdict .= "<br><br><strong>Related Cases:</strong><br>" . implode('<br>', $relatedCases);
     $suggestedVerdict .= "<br><br><strong>Advice to the Community:</strong><br>" . $adviceToCommunity;
     
-    header("Location: ..admin/recommender_system.php");
+    header("Location: ../admin/recommender_system.php?success=1");
     exit();
     
 
